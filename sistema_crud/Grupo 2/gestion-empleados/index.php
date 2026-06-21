70f7690965503b42fd28a5086dbcdec6b9b5a9fb
JosueC0de <josue98.wst@gmail.com>
2026-06-21 15:26:25 -0600

    Update index.php

--- sistema_crud/Grupo 2/gestion-empleados/index.php
+++ sistema_crud/Grupo 2/gestion-empleados/index.php
@@ -3,3 +3,18 @@
     require_once "controladores/EmpleadoController.php";
     $action = isset($_GET['action']) ? $_GET['action'] : 'inicio';
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Control de Empleados</title>
+
+</head>
+<body>
+    <div >
+       
+    </div>
+    
+</body>
+</html>
